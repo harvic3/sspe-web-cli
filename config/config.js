3b00a0fe2412de07993a6d3029f38ee52f7679ff
Vickodev <harvic3@gmail.com>
2019-08-03 21:00:52 -0500

    change api url

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
   env: process.env || 'DEV',
   DEV_SERVICE: "http://localhost:3030",
-  PROD_SERVICE: "https://sspe-project.vickodev.now.sh"
+  PROD_SERVICE: "sspe-project-api.vickodev.now.sh"
 };
 
 exports.config = config;
