4cf447dead475fdc247d67bd3a63c7e36c6d444e
Vickodev <harvic3@gmail.com>
2020-06-13 02:34:16 -0500

    change http to https

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
   DEV_SERVICE: "http://localhost:3030",
-  PROD_SERVICE: "http://sspeapi.devspace.ml"
+  PROD_SERVICE: "https://sspeapi.devspace.ml"
 };
 
 exports.config = config;
